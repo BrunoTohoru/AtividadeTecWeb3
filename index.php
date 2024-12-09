78679eccfc9dbe12cc3fbcb434dc274acd963611
Bruno Shiotani <bruno_shiotani@outlook.com>
2024-12-09 00:00:20 -0300

    08/12

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../../cli-config.php';
+use Entities\Livro;
+$livro = new Livro();
+// comando para executar/criar o banco: php vendor/bin/doctrine orm:schema-tool:create
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
